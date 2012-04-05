e0b37a5444c88930a01b2f65623b046515672180
Mario Albert <Mario.Albert@gmx.de>
2012-04-05 15:04:48 +0200

    fix-for-bootstrap

--- app/assets/javascripts/multi_input_line/multi_input.js
+++ app/assets/javascripts/multi_input_line/multi_input.js
@@ -1,13 +1,14 @@
 jQuery(function($){
   $('*[data-validation-error]').each(function(){
     var input = $(this);
-    var parent = input.parent("div.multi");
+    var parent = input.parents("div.multi");
     wrapper = $('<div style="display: inline-block; vertical-align: top"></div>');
     input.replaceWith(wrapper);
     wrapper.append(input);
     parent.addClass('field_with_errors');
+    parent.addClass('error');
     var message = input.data('validation-error');
-    var span = $('<span class="error"></span>');
+    var span = $('<span class="error help-inline" style="display:block"></span>');
     span.text(message);
     wrapper.append(span);
   });
