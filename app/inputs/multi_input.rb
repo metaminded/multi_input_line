5c9a40fcffa682caeff8a28852b8ca4968a13bdf
Peter Horn <peter.horn@provideal.net>
2012-06-19 16:29:11 +0200

    labels

--- app/inputs/multi_input.rb
+++ app/inputs/multi_input.rb
@@ -4,6 +4,11 @@ class MultiInput < SimpleForm::Inputs::Base
     fields = options[:fields]
     fields.map do |name, opts|
       lbl = opts.delete :label
+      lbl = case lbl
+      when String then lbl
+      when Symbol then @builder.object.human_attribute_name(lbl)
+      else nil
+      end
       e = @builder.object.errors[name].presence.try(:join, ", ")
       opts = opts.merge('data-validation-error' => e) if e
       f = @builder.input_field name, opts
